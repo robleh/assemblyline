dde1b456647c47ec060c6bad1464c21cc3ef224f
Robleh <roblehesa@gmail.com>
2025-03-24 04:29:17 -0400

    Remove response file generation

--- cmake/AssemblyLine.cmake
+++ cmake/AssemblyLine.cmake
@@ -18,26 +18,9 @@ if(NOT DEFINED AL_ENTRY)
     set(AL_ENTRY entry)
 endif()
 
-if(NOT DEFINED AL_MERGE)
-    set(AL_MERGE /merge:.al=.text /merge:.text=.pic)
-endif()
-
-if(AL_MERGE_RDATA)
-    set(AL_MERGE /merge:.rdata=.al ${AL_MERGE})
-    message(STATUS "Assembly Line: Linker will merge .rdata with code")
-endif()
-
-if(AL_MERGE_DATA)
-    set(AL_MERGE /merge:.data=.al ${AL_MERGE})
-    message(STATUS "Assembly Line: Linker will merge .data with code")
-endif()
-
 ##############################################################################
 # Toolchain Option Interfaces
 ##############################################################################
-# Directory that will contain the response files for build tools.
-set(AL_TOOLCHAIN_DIR ${CMAKE_CURRENT_BINARY_DIR}/al)
-
 add_library(al-compiler INTERFACE)
 add_library(al::compiler ALIAS al-compiler)
 set_target_properties(al-compiler PROPERTIES EXPORT_NAME compiler)
@@ -110,8 +93,11 @@ if (${CMAKE_CXX_COMPILER_ID} STREQUAL MSVC)
     target_compile_options(
         al-compiler
         INTERFACE
-            @${AL_TOOLCHAIN_DIR}/$<CONFIG>/compiler-options
+            ${AL_CXX_FLAGS}
+            $<$<CONFIG:Debug>:${AL_CXX_FLAGS_DEBUG}>
+            $<$<CONFIG:Release>:${AL_CXX_FLAGS_RELEASE}>
     )
+
 elseif (${CMAKE_CXX_COMPILER_ID} STREQUAL Clang)
     if (NOT DEFINED AL_CXX_FLAGS)
         set(
@@ -141,14 +127,14 @@ elseif (${CMAKE_CXX_COMPILER_ID} STREQUAL Clang)
 
     al_disable_msvcrt_directives()
 
-    # The @ prefix for response files was breaking using Windows encoding. If
-    # that can be fixed all 3 compilers can be converged into one command.
     target_compile_options(
         al-compiler
-        BEFORE
         INTERFACE
-            --config=${AL_TOOLCHAIN_DIR}/$<CONFIG>/compiler-options
+            ${AL_CXX_FLAGS}
+            $<$<CONFIG:Debug>:${AL_CXX_FLAGS_DEBUG}>
+            $<$<CONFIG:Release>:${AL_CXX_FLAGS_RELEASE}>
     )
+
 elseif (${CMAKE_CXX_COMPILER_ID} STREQUAL GNU)
     if (NOT DEFINED AL_CXX_FLAGS)
         set(
@@ -179,44 +165,46 @@ elseif (${CMAKE_CXX_COMPILER_ID} STREQUAL GNU)
 
     al_disable_msvcrt_directives()
 
-    # The @ prefix for a response files was breaking command-line encoding for
-    # some reason. Unfortunately, that means the compiler options must be
-    # configured for each toolchain.
     target_compile_options(
         al-compiler
-        BEFORE
         INTERFACE
-            @${AL_TOOLCHAIN_DIR}/$<CONFIG>/compiler-options
+            ${AL_CXX_FLAGS}
+            $<$<CONFIG:Debug>:${AL_CXX_FLAGS_DEBUG}>
+            $<$<CONFIG:Release>:${AL_CXX_FLAGS_RELEASE}>
     )
+
 else()
     message(FATAL_ERROR "${CMAKE_CXX_COMPILER_ID} compiler not supported")
-endif()
 
-file(
-    GENERATE
-    OUTPUT
-        ${AL_TOOLCHAIN_DIR}/$<CONFIG>/compiler-options
-    CONTENT
-        "$<JOIN:\
-${AL_CXX_FLAGS} \
-$<$<CONFIG:Debug>:${AL_CXX_FLAGS_DEBUG}>\
-$<$<CONFIG:Release>:${AL_CXX_FLAGS_RELEASE}>\
-, >"
-)
+endif()
 
 ##############################################################################
 # Linker Configuration
 ##############################################################################
 if (${CMAKE_CXX_COMPILER_LINKER_ID} STREQUAL MSVC)
+    if(NOT DEFINED AL_MERGE)
+        set(AL_MERGE /merge:.al=.text /merge:.text=.pic)
+    endif()
+
+    if(AL_MERGE_RDATA)
+        set(AL_MERGE /merge:.rdata=.al ${AL_MERGE})
+        message(STATUS "Assembly Line: Linker will merge .rdata with code")
+    endif()
+
+    if(AL_MERGE_DATA)
+        set(AL_MERGE /merge:.data=.al ${AL_MERGE})
+        message(STATUS "Assembly Line: Linker will merge .data with code")
+    endif()
+
     if (NOT DEFINED AL_EXE_LINKER_FLAGS)
         set(
             AL_EXE_LINKER_FLAGS
+            /machine:x64
             /nologo
             /nodefaultlib
             /map
             /manifest:no
             /emittoolversioninfo:no
-            /entry:${AL_ENTRY}
             ${AL_MERGE}
         )
     endif()
@@ -239,14 +227,33 @@ if (${CMAKE_CXX_COMPILER_LINKER_ID} STREQUAL MSVC)
 
     target_link_options(
         al-linker
-        BEFORE
         INTERFACE
-            @${AL_TOOLCHAIN_DIR}/$<CONFIG>/linker-options
+            $<$<STREQUAL:EXECUTABLE,$<TARGET_PROPERTY:TYPE>>:/entry:${AL_ENTRY}>
+            $<$<STREQUAL:SHARED_LIBRARY,$<TARGET_PROPERTY:TYPE>>:/noentry>
+            ${AL_EXE_LINKER_FLAGS}
+            $<$<CONFIG:Debug>:${AL_EXE_LINKER_FLAGS_DEBUG}>
+            $<$<CONFIG:Release>:${AL_EXE_LINKER_FLAGS_RELEASE}>
     )
+
 elseif (${CMAKE_CXX_COMPILER_LINKER_ID} STREQUAL LLD)
+    if(NOT DEFINED AL_MERGE)
+        set(AL_MERGE /merge:.al=.text /merge:.text=.pic)
+    endif()
+
+    if(AL_MERGE_RDATA)
+        set(AL_MERGE /merge:.rdata=.al ${AL_MERGE})
+        message(STATUS "Assembly Line: Linker will merge .rdata with code")
+    endif()
+
+    if(AL_MERGE_DATA)
+        set(AL_MERGE /merge:.data=.al ${AL_MERGE})
+        message(STATUS "Assembly Line: Linker will merge .data with code")
+    endif()
+
     if (NOT DEFINED AL_EXE_LINKER_FLAGS)
         set(
             AL_EXE_LINKER_FLAGS
+            /machine:x64
             /nologo
             /nodefaultlib
             /map
@@ -257,7 +264,7 @@ elseif (${CMAKE_CXX_COMPILER_LINKER_ID} STREQUAL LLD)
 
     if (NOT DEFINED AL_EXE_LINKER_FLAGS_DEBUG)
         set(
-            AL_CXX_FLAGS_DEBUG
+            AL_EXE_LINKER_FLAGS_DEBUG
             /debug
             /opt:ref
         )
@@ -271,26 +278,19 @@ elseif (${CMAKE_CXX_COMPILER_LINKER_ID} STREQUAL LLD)
         )
     endif()
 
+    # CMake passes lld-link options via clang.exe using the -Xlinker flag. In
+    # order to properly prefix our parameters the LINKER: modifier must be
+    # specified. This modifier expects comma separated items.
     target_link_options(
         al-linker
-        BEFORE
         INTERFACE
-            -Wl,@${AL_TOOLCHAIN_DIR}/$<CONFIG>/linker-options
+            $<$<STREQUAL:EXECUTABLE,$<TARGET_PROPERTY:TYPE>>:LINKER:/entry:${AL_ENTRY}>
+            $<$<STREQUAL:SHARED_LIBRARY,$<TARGET_PROPERTY:TYPE>>:LINKER:/noentry>
+            LINKER:$<JOIN:${AL_EXE_LINKER_FLAGS},,>
+            $<$<CONFIG:Debug>:LINKER:$<JOIN:${AL_EXE_LINKER_FLAGS_DEBUG},,>>
+            $<$<CONFIG:Release>:LINKER:$<JOIN:${AL_EXE_LINKER_FLAGS_RELEASE},,>>
     )
 
-    target_link_options(
-        al-linker-exe
-        BEFORE
-        INTERFACE
-            /entry:${AL_ENTRY}
-    )
-
-    target_link_options(
-        al-linker
-        BEFORE
-        INTERFACE
-            /noentry
-    )
 elseif (${CMAKE_CXX_COMPILER_LINKER_ID} STREQUAL GNU)
     if (NOT DEFINED AL_EXE_LINKER_FLAGS)
         set(
@@ -303,7 +303,7 @@ elseif (${CMAKE_CXX_COMPILER_LINKER_ID} STREQUAL GNU)
     endif()
 
     if (NOT DEFINED AL_EXE_LINKER_FLAGS_DEBUG)
-        set(AL_CXX_FLAGS_DEBUG)
+        set(AL_EXE_LINKER_FLAGS_DEBUG)
     endif()
 
     if (NOT DEFINED AL_EXE_LINKER_FLAGS_RELEASE)
@@ -317,7 +317,12 @@ elseif (${CMAKE_CXX_COMPILER_LINKER_ID} STREQUAL GNU)
         al-linker
         BEFORE
         INTERFACE
-            -nostdlib -Wl,@${AL_TOOLCHAIN_DIR}/$<CONFIG>/linker-options
+            -nostdlib
+            $<$<STREQUAL:EXECUTABLE,$<TARGET_PROPERTY:TYPE>>:--entry=${AL_ENTRY}>
+            $<$<STREQUAL:SHARED_LIBRARY,$<TARGET_PROPERTY:TYPE>>:--no-entry>
+            ${AL_EXE_LINKER_FLAGS}
+            $<$<CONFIG:Debug>:${AL_EXE_LINKER_FLAGS_DEBUG}>
+            $<$<CONFIG:Release>:${AL_EXE_LINKER_FLAGS_RELEASE}>
     )
 
     file(
@@ -341,21 +346,11 @@ elseif (${CMAKE_CXX_COMPILER_LINKER_ID} STREQUAL GNU)
   }\n
 }"
 )
+
 else()
     message(FATAL_ERROR "${CMAKE_CXX_COMPILER_ID} linker not supported")
-endif()
 
-file(
-    GENERATE
-    OUTPUT
-        ${AL_TOOLCHAIN_DIR}/$<CONFIG>/linker-options
-    CONTENT
-        "$<JOIN:\
-${AL_EXE_LINKER_FLAGS} \
-$<$<CONFIG:Debug>:${AL_EXE_LINKER_FLAGS_DEBUG}>
-$<$<CONFIG:Release>:${AL_EXE_LINKER_FLAGS_RELEASE}>
-        , >"
-)
+endif()
 
 ##############################################################################
 # Assembler Configuration
@@ -373,15 +368,23 @@ if (DEFINED ${CMAKE_ASM_MASM_COMPILER_ID} AND ${CMAKE_ASM_MASM_COMPILER_ID} STRE
 
     if (NOT DEFINED AL_ASM_MASM_FLAGS_DEBUG)
         set(
-            AL_CXX_FLAGS_DEBUG
+            AL_ASM_MASM_FLAGS_DEBUG
         )
     endif()
 
     if (NOT DEFINED AL_ASM_MASM_FLAGS_RELEASE)
         set(
-            AL_EXE_LINKER_FLAGS_RELEASE
+            AL_ASM_MASM_FLAGS_RELEASE
         )
     endif()
+
+    target_compile_options(
+        al-assembler
+        INTERFACE
+            ${AL_ASM_MASM_LINKER_FLAGS}
+            $<$<CONFIG:Debug>:${AL_ASM_MASM_LINKER_FLAGS_DEBUG}>
+            $<$<CONFIG:Release>:${AL_ASM_MASM_LINKER_FLAGS_RELEASE}>
+    )
 
 # Off brand MASM compatible compilers don't have compiler ID defined.
 elseif (NOT DEFINED ${CMAKE_ASM_MASM_COMPILER_ID})
@@ -395,37 +398,29 @@ elseif (NOT DEFINED ${CMAKE_ASM_MASM_COMPILER_ID})
 
         if (NOT DEFINED AL_ASM_MASM_FLAGS_DEBUG)
             set(
-                AL_CXX_FLAGS_DEBUG
+                AL_ASM_MASM_FLAGS_DEBUG
             )
         endif()
 
         if (NOT DEFINED AL_ASM_MASM_FLAGS_RELEASE)
             set(
-                AL_EXE_LINKER_FLAGS_RELEASE
+                AL_ASM_MASM_LINKER_FLAGS_RELEASE
             )
         endif()
     endif()
+
+    target_compile_options(
+        al-assembler
+        INTERFACE
+            ${AL_ASM_MASM_LINKER_FLAGS}
+            $<$<CONFIG:Debug>:${AL_ASM_MASM_LINKER_FLAGS_DEBUG}>
+            $<$<CONFIG:Release>:${AL_ASM_MASM_LINKER_FLAGS_RELEASE}>
+    )
+
 else()
     message(FATAL_ERROR "${CMAKE_ASM_MASM_COMPILER} assembler not supported")
+
 endif()
-
-target_compile_options(
-    al-assembler
-    INTERFACE
-        @${AL_TOOLCHAIN_DIR}/$<CONFIG>/assembler-options
-)
-
-file(
-    GENERATE
-    OUTPUT
-        ${AL_TOOLCHAIN_DIR}/$<CONFIG>/assembler-options
-    CONTENT
-        "$<JOIN:\
-        ${AL_ASM_MASM_FLAGS}\
-        $<$<CONFIG:Debug>:${AL_ASM_MASM_FLAGS_DEBUG}>\
-        $<$<CONFIG:Release>:${AL_ASM_MASM_FLAGS_RELEASE}>\
-        , >"
-)
 
 ##############################################################################
 # Functions and Macros
@@ -516,6 +511,7 @@ macro(al_mask_flags)
     set(CMAKE_C_FLAGS_RELEASE "")
     set(CMAKE_C_FLAGS_MINSIZEREL "")
     set(CMAKE_C_FLAGS_RELWITHDEBINFO "")
+    set(CMAKE_EXE_LINKER_FLAGS "")
     set(CMAKE_EXE_LINKER_FLAGS_DEBUG "")
     set(CMAKE_EXE_LINKER_FLAGS_RELEASE "")
     set(CMAKE_SHARED_LINKER_FLAGS_DEBUG "")
